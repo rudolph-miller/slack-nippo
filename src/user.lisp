be35403dfe048887a58537ce2e764e1e7ede3943
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-11-12 21:54:49 +0900

    Add get-all-users and get-user

--- src/user.lisp
+++ src/user.lisp
@@ -8,3 +8,31 @@
 (in-package :slack-nippo.user)
 
 (syntax:use-syntax :annot)
+
+@export
+@export-structure
+(defstruct (user (:constructor %make-user))
+  id
+  name)
+
+@export
+(defun make-user (user)
+  (%make-user :id (assoc-value "id" user)
+              :name (assoc-value "name" user)))
+
+(defparameter *users* nil)
+
+@export
+(defun get-all-users (&optional (cache t))
+  (if (and cache *users*)
+      *users*
+      (let ((users (assoc-value "members" (fetch "/users.list"))))
+        (setf *users*
+              (mapcar #'(lambda (user) (make-user user))
+                      users)))))
+
+@export
+(defun get-user (id)
+  (find id (get-all-users)
+        :key #'user-id
+        :test #'equal))
