dd0427897a2e303c2e43a3c43f2305485c762025
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-11-12 21:00:50 +0900

    Add get-all-channels

--- src/channel.lisp
+++ src/channel.lisp
@@ -1,7 +1,22 @@
 (in-package :cl-user)
 (defpackage :slack-nippo.channel
   (:use :cl
-        :annot.doc))
+        :annot.doc
+        :annot.class
+        :slack-nippo.util
+        :slack-nippo.fetch))
 (in-package :slack-nippo.channel)
 
 (syntax:use-syntax :annot)
+
+@export
+@export-structure
+(defstruct channel
+  id name)
+
+(defun get-all-channels ()
+  (let ((channels (assoc-value "channels" (fetch "/channels.list"))))
+    (mapcar #'(lambda (channel)
+                (make-channel :id (assoc-value "id" channel)
+                              :name (assoc-value "name" channel)))
+            channels)))
